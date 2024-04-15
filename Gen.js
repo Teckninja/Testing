40482111db394237613806ee8eadf02f6213be79
Teckninja <167083499+Teckninja@users.noreply.github.com>
2024-04-15 06:06:08 -0500

    Create Gen.js
    Just testing

---
+++ Gen.js
@@ -0,0 +1,4 @@
+var account_gen = new Array("Most American car horns honk in the key of F.",
+    "The name Wendy was made up for the book 'Peter Pan.'")
+module.exports = {
+    account_gen: account_gen};
